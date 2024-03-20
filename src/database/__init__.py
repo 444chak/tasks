0e99aa8cc8b8b51d50420104de82eafb92daa453
444chak <444chak@gmail.com>
2024-03-20 21:12:10 +0100

    Task CLI system operational (CRUD)

---
+++ src/database/__init__.py
@@ -0,0 +1,75 @@
+"""Database module for the application."""
+
+from datetime import date
+import sqlite3
+import click
+
+DATABASE = sqlite3.connect("database.db")
+
+
+
+def init():
+    """Initialize the database."""
+    DATABASE.execute(
+        "CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, task TEXT, end_date DATE, done BOOLEAN)"
+    )
+    DATABASE.commit()
+    click.echo("Initialized the database.")
+
+if DATABASE.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall() == []:
+    init()
+
+def show_tables():
+    """Show all tables in the database."""
+    cursor = DATABASE.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    return cursor.fetchall()
+
+
+def add_task(task: str, end_date: date):
+    """Add a task to the database."""
+    try:
+        DATABASE.execute(
+            "INSERT INTO tasks (task, end_date, done) VALUES (?, ?, ?)",
+            (task, end_date, False),
+        )
+        DATABASE.commit()
+        return True
+    except sqlite3.Error as e:
+        click.echo(f"An error occurred: {e}")
+        return False
+
+def remove_task(task_id: int):
+    """Remove a task from the database."""
+    try:
+        cursor = DATABASE.cursor()
+        cursor.execute("DELETE FROM tasks WHERE id=?", (task_id,))
+        if cursor.rowcount == 0:
+            return False
+        DATABASE.commit()
+        return True
+    except sqlite3.Error as e:
+        click.echo(f"An error occurred: {e}")
+        return False
+
+def update_task(task_id: int, done: bool):
+    """Update the done status of a task."""
+    try:
+        DATABASE.execute("UPDATE tasks SET done=? WHERE id=?", (done, task_id))
+        DATABASE.commit()
+        return True
+    except sqlite3.Error as e:
+        click.echo(f"An error occurred: {e}")
+        return False
+
+def get_task(task_id: int):
+    """Get a task from the database."""
+    cursor = DATABASE.cursor()
+    cursor.execute("SELECT * FROM tasks WHERE id=?", (task_id,))
+    return cursor.fetchone()
+
+def tasks_list():
+    """Show all tasks in the database."""
+    cursor = DATABASE.cursor()
+    cursor.execute("SELECT * FROM tasks")
+    return cursor.fetchall()
--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,35 +1,88 @@
 """The main module for the tasks package."""
 
-import uuid
+import re
+from datetime import date
 from dataclasses import dataclass
 import click
+import database
+
 
 
 @dataclass
 class Task:
     """A simple class to represent a task."""
-    id: uuid.UUID
+    id: int
     task: str
+    end_date: date
+    done: bool = False
 
+    def __post_init__(self):
+        self.end_date = date.fromisoformat(self.end_date)
+
+
+    def __str__(self):
+        status = "[X]" if self.done else "[ ]"
+        return f"{self.id}. {status} \t📅 {self.end_date.strftime('%d/%m/%Y')} \t📝 {self.task} "
+
+    def check(self):
+        """Mark the task as done if it is not already, if it is, mark it as not done."""
+        self.done = not self.done
+        database.update_task(self.id, self.done)
 
 @click.group()
 def cli():
     """A simple CLI for managing tasks."""
 
 
+
+@cli.command()
+def todo():
+    """List all tasks."""
+    click.echo("Tasks:")
+    for task in database.tasks_list():
+        click.echo(Task(*task))
+
 @cli.command()
 @click.option("-t", "--task", prompt="Your task", help="The task to remember.")
-def display(task: str):
-    """Display a task."""
-    todo = Task(uuid.uuid4(), task)
-    click.echo(todo)
+@click.option("-d", "--end_date", prompt="End date", help="The end date of the task.")
+def add(task: str, end_date: str):
+    """Add a task.
+    TASK is the description of the task.
+    END_DATE is the end date of the task in the format dd/mm/yyyy."""
 
+    if not re.match(r"\d{2}\/\d{2}\/\d{4}", end_date):
+        click.echo("The date must be in the format dd/mm/yyyy.")
+        return
+    day, month, year = map(int, end_date.split("/"))
+    end_date = date(year, month, day)
+    if end_date < date.today():
+        click.echo("The date must be in the future.")
+        return
 
+    if database.add_task(task, end_date):
+        click.echo("Task added ! ✅")
+        click.echo(f"Task: {task}, End date: {end_date.strftime('%d/%m/%Y')}")
 
-@click.command()
-@click.option("--count", default=1)
-@click.option("--name", prompt="Your name")
-def hello(count, name):
-    """Print hello NAME for COUNT times."""
-    for _ in range(count):
-        click.echo(f"Hello {name}!")
+@cli.command()
+@click.argument("task_id", type=int, required=True)
+def remove(task_id: int):
+    """Remove a task.
+    TASK_ID is the number of the task to remove.
+    If the task does not exist, an error message will be displayed.
+    For knowing the task_id, use the list command."""
+    if database.remove_task(task_id):
+        click.echo(f"Task {task_id} removed ! ✅")
+    else:
+        click.echo(f"Task {task_id} not found ! ❌")
+
+@cli.command()
+@click.argument("task_id", type=int, required=True)
+def done(task_id: int):
+    """Mark a task as done.
+    TASK_ID is the number of the task to mark as done.
+    If the task does not exist, an error message will be displayed.
+    For knowing the task_id, use the list command."""
+    task = Task(*database.get_task(task_id))
+    if task:
+        task.check()
+        click.echo(f"Task {task_id} updated ! ✅")
